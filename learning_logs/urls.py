f79ddc5425519e8f7b6ea6ab55a12272c35c14a3
Andrew Clarke <andrewneilclarke@gmail.com>
2021-02-03 16:57:08 +0100

    added topic page to learning app

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,5 +8,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
-    # Page that shows a
+    # Page that shows all topics
+    path('/topics', views.topics, name='topics'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 def index(request):
     """The homepage for Learning Log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """ show all topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
