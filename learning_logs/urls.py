95b1e8813dede9c845e949ecffc6a9126fd4319d
Andrew Clarke <andrewneilclarke@gmail.com>
2021-02-04 13:39:22 +0100

    added add and edit functionality

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,5 +9,13 @@ urlpatterns = [
     # Home page
     path('', views.index, name='index'),
     # Page that shows all topics
-    path('/topics', views.topics, name='topics'),
+    path('topics/', views.topics, name='topics'),
+    #Detail page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    #page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # page for adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # page for editing a new entry
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
