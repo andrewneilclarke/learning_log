3698114bb26fe5e99a6203608ad7b8f0de47efe0
Andrew Clarke <andrewneilclarke@gmail.com>
2021-02-05 09:47:25 +0100

    update to user registration

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -6,18 +7,22 @@ def index(request):
     """The homepage for Learning Log"""
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
     """ show all topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """ show a single topic and all its entries"""
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+@login_required
 def new_topic(request):
     """Add a new topic."""
     if request.method != 'POST':
@@ -33,6 +38,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry."""
     topic = Topic.objects.get(id=topic_id)
@@ -52,7 +58,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
-
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,13 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     # include default auth urls
     path('', include('django.contrib.auth.urls')),
+    #registration page
+    path('register/', views.register, name='register'),
+
 ]
